1d046ef9b5910406708d97c8c01ef52a4cb224e1
Baoyuan Wu <wubaoyuan@users.noreply.github.com>
2018-10-12 21:09:25 +0800

    download_url_tiny

---
+++ data/download_im_from_url_new.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import sys
+import urllib
+import argparse
+
+def main(url_list, save_dir):
+    i=0
+    with open(url_list, 'r') as lines:
+        for line in lines:
+            sp = line.rstrip('\n').split('\t')
+            url = sp[0]
+            urllib.urlretrieve(url, save_dir + 'im_'+str(i)+'.png')
+            i += 1
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url_list', type=str, help='the url list file')
+    parser.add_argument('--save_dir', type=str, default='./', help='the directory to save images')
+    args = parser.parse_args()
+
+    main(url_list=args.url_list, save_dir=args.save_dir)
