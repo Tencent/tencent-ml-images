8902999626ca23ec7bc099a818696fb26c32d621
Baoyuan Wu <wubaoyuan@users.noreply.github.com>
2018-10-22 18:27:08 +0800

    Add files via upload

---
+++ example/image_classification.sh
@@ -0,0 +1,5 @@
+python2.7 image_classification.py \
+ --images=data/im_list_for_classification.txt \
+ --top_k_pred=5 \
+ --model_dir=checkpoints/resnet.ckpt-50000 \
+ --dictionary=data/imagenet2012_dictionary.txt
