297ee9fb1e4b4b3f72e97e183e4589bd0ee024c1
Baoyuan Wu <wubaoyuan@users.noreply.github.com>
2018-10-24 22:00:34 +0800

    Update image_classification.sh

--- example/image_classification.sh
+++ example/image_classification.sh
@@ -1,5 +1,5 @@
 python2.7 image_classification.py \
  --images=data/im_list_for_classification.txt \
  --top_k_pred=5 \
- --model_dir=checkpoints/resnet.ckpt-50000 \
+ --model_dir=checkpoints/resnet.ckpt \
  --dictionary=data/imagenet2012_dictionary.txt
